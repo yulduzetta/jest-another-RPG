4e0b3e189641385c903c1b32b4135e830d20d055
yulduz <yulduz83@gmail.com>
2021-02-10 22:49:54 -0600

    health and specs

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -35,3 +35,29 @@ test("gets inventory from player or returns false", () => {
   player.inventory = [];
   expect(player.getInventory()).toEqual(false);
 });
+
+test("gets information about player's health", () => {
+  const player = new Player("Dave");
+
+  expect(player.getHealth()).toEqual(
+    expect.stringContaining(player.health.toString())
+  );
+});
+
+test("checks if player is alive or not", () => {
+  const player = new Player("Dave");
+
+  expect(player.isAlive()).toBeTruthy();
+  player.health = 0;
+  expect(player.isAlive()).toBeFalsy();
+});
+
+test("substracts from player's health", () => {
+  const player = new Player("Dave");
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+  expect(player.health).toBe(oldHealth - 5);
+  player.reduceHealth(99999);
+  expect(player.health).toBe(0);
+});
